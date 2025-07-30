6c45becc9b45efa6e1aea11fb6750d0d06253758
rishabizzback <bgmipersonalinfo@gmail.com>
2025-07-30 15:24:14 +0530

    Update shivam.py

--- shivam.py
+++ shivam.py
@@ -21,8 +21,8 @@ redeem_codes_collection = db['redeem_codes']
 attack_logs_collection = db['user_attack_logs']
 
 # Bot Configuration
-TELEGRAM_BOT_TOKEN = '7456867887:AAEDbt0W6pUy7J_8laF1OEEmIxFKwSv6ZYA'
-ADMIN_USER_ID = 7814078698 
+TELEGRAM_BOT_TOKEN = '7219302227:AAHUz5hwIsJefweWl8nINUAyBQjdn0n55wI'
+ADMIN_USER_ID = 5976309679
 COOLDOWN_PERIOD = timedelta(minutes=1) 
 user_last_attack_time = {} 
 user_attack_history = {}
